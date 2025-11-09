4ef1e9b2c76090874db1961bf26feac0274941ba
Zmxcrr <pik77905@gmail.com>
2025-11-10 01:47:49 +0300

    fix: path to img

--- src/components/search/LocationBadge.tsx
+++ src/components/search/LocationBadge.tsx
@@ -13,7 +13,7 @@ export const LocationBadge: React.FC<LocationBadgeProps> = ({ onClick }) => {
             onClick={onClick}
             className="h-full flex items-center gap-2 px-6 py-3 bg-green-500  text-white rounded-full shadow-lg hover:bg-green-600 transition-colors"
         >
-            <img src={'src/img/icons/curr_loc.svg'} className="w-[25px] h-[25px]" alt={t.currentLocation}/>
+            <img src="/icons/curr_loc.svg" className="w-[25px] h-[25px]" alt={t.currentLocation}/>
             <span className="font-extrabold text-base">{t.currentLocation}</span>
         </button>
     );
--- src/components/weather/CurrentWeatherCard.tsx
+++ src/components/weather/CurrentWeatherCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CurrentWeather, Units } from '../../types/weather';
+
 import {useI18n} from "../../hooks/useI18n.ts";
 import {formatTime} from "../../utils/weatherUtils.ts";
 import {WeatherIcon} from "./icons/WeatherIcons.tsx";
@@ -30,7 +31,7 @@ export const CurrentWeatherCard: React.FC<CurrentWeatherCardProps> = ({ weather,
 
                 <div className="grid-cols-1 mt-3">
                     <div className="flex items-center gap-4 mb-4">
-                        <img src={'src/img/icons/sunrise.svg'} className="dark:invert w-[48px] h-[48px]" alt={t.sunrise}/>
+                        <img src="/icons/sunrise.svg" className="dark:invert w-[48px] h-[48px]" alt={t.sunrise}/>
                         <div>
                             <div className="text-sm opacity-70 mb-1">{t.sunrise}</div>
                             <div className="text-base font-semibold">
@@ -40,7 +41,7 @@ export const CurrentWeatherCard: React.FC<CurrentWeatherCardProps> = ({ weather,
                     </div>
 
                     <div className="flex items-center gap-4">
-                        <img src={'src/img/icons/sunset.svg'} className="dark:invert w-[48px] h-[48px]" alt={t.sunset}/>
+                        <img src="/icons/sunset.svg" className="dark:invert w-[48px] h-[48px]" alt={t.sunset}/>
                         <div>
                             <div className="text-sm opacity-70 mb-1">{t.sunset}</div>
                             <div className="text-base font-semibold">
@@ -60,28 +61,28 @@ export const CurrentWeatherCard: React.FC<CurrentWeatherCardProps> = ({ weather,
 
             <div className="grid grid-cols-2 grid-rows-2">
                 <div className="grid grid-cols-1 place-items-center">
-                    <img src={'src/img/icons/humidity.svg'} className="dark:invert" alt={t.humidity}/>
+                    <img src="/icons/humidity.svg" className="dark:invert" alt={t.humidity}/>
                     <div>
                         <div className="font-semibold text-xl text-center">{weather.main.humidity}%</div>
                         <div className="font-medium text-base mb-1 text-center">{t.humidity}</div>
                     </div>
                 </div>
                 <div className="grid grid-cols-1 place-items-center">
-                    <img src={'src/img/icons/wind.svg'} className="dark:invert" alt={t.windSpeed}/>
+                    <img src="/icons/wind.svg" className="dark:invert" alt={t.windSpeed}/>
                     <div>
                         <div className="font-semibold text-xl text-center">{Math.round(weather.wind.speed)}{speedUnit === 'km/h' ? t.kmh : t.mph}</div>
                         <div className="font-medium text-base mb-1 text-center">{t.windSpeed}</div>
                     </div>
                 </div>
                 <div className="grid grid-cols-1 place-items-center">
-                    <img src={'src/img/icons/pressure.svg'} className="dark:invert" alt={t.pressure}/>
+                    <img src="/icons/pressure.svg" className="dark:invert" alt={t.pressure}/>
                     <div>
                         <div className="font-semibold text-xl text-center">{weather.main.pressure}{t.hPa}</div>
                         <div className="font-medium text-base mb-1 text-center">{t.pressure}</div>
                     </div>
                 </div>
                 <div className="grid grid-cols-1 place-items-center">
-                    <img src={'src/img/icons/uv.svg'} className="dark:invert" alt={t.uv}/>
+                    <img src="/icons/uv.svg" className="dark:invert" alt={t.uv}/>
                     <div>
                         <div className="font-semibold text-xl text-center">{uvIndex}</div>
                         <div className="font-medium text-base mb-1 text-center">{t.uv}</div>
